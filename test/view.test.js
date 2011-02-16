a760580856fc8a55f5f35c82bce71aff76787268
Zach Smith <x.coder.zach@gmail.com>
2011-02-15 22:15:58 -0600

    added iterables

--- test/view.test.js
+++ test/view.test.js
@@ -9,4 +9,23 @@
     assertEquals("Variable was not set", $("#template .variable").html(), "value");
   };
 
+  ViewTest.prototype.testAddIterable = function() {
+    /*:DOC += <div id = "templateIterable">
+                <ul class = "things">
+                  <li class = "thing">
+                    <span class = "variable"></span>
+                  </li>
+                </ul>
+              </div>*/
+    var view = new LiveView($("#templateIterable").get(0));
+
+
+    view.add("thing", {"variable": "value"});
+    view.add("thing", {"variable": "value2"});
+
+    jstestdriver.console.log($("#templateIterable").get(0).innerHTML);
+
+    assertEquals("Variable was not set", $("#templateIterable li:nth-child(1) .variable").html(), "value");
+    assertEquals("Variable was not set", $("#templateIterable li:nth-child(2) .variable").html(), "value2");
+  }; 
 }());
