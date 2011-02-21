baa775c6beab5353aa2e4b9a606ac8e0600fd34d
Zach Smith <x.coder.zach@gmail.com>
2011-02-20 22:19:24 -0600

    whoa lots of stuff

--- test/view.test.js
+++ test/view.test.js
@@ -4,9 +4,16 @@
   ViewTest.prototype.testSetVariable = function() {
     /*:DOC += <div id = "template"><span class = "variable"></span></div>*/
     var view = new LiveView($("#template").get(0));
-    view.update("variable", "value");
+    view.set("variable", "value");
     assertEquals("Variable was not set", $("#template .variable").html(), "value");
   };
+
+  ViewTest.prototype.testSetAttribute = function() {
+    /*:DOC += <div id = "template"><a class = "variable"></a></div>*/
+    var view = new LiveView($("#template"));
+    view.set("variable", {"content":"googs", "href": "http://google.com"});
+    assertEquals("Variable was not set", $("#template .variable").attr("href"), "http://google.com");
+  }; 
 
   ViewTest.prototype.testAddIterable = function() {
     /*:DOC += <div id = "templateIterable">
@@ -16,46 +23,101 @@
                   </li>
                 </ul>
               </div>*/
-    var view = new LiveView($("#templateIterable").get(0));
+    var template = new LiveView("#templateIterable", {
+      "things": [{"variable": "A variable"}]
+    });
 
-    view.add("thing", {"variable": "value"});
-    view.add("thing", {"variable": "value2"});
+    template.things.add({"variable": "A value"});
 
-    assertEquals("Variable was not set", $("#templateIterable li:nth-child(1) .variable").html(), "value");
-    assertEquals("Variable was not set", $("#templateIterable li:nth-child(2) .variable").html(), "value2");
+    assertEquals("Variable was not set", $("#templateIterable li:nth-child(1) .variable").html(), "A variable");
+    assertEquals("Variable was not set", $("#templateIterable li:nth-child(2) .variable").html(), "A value");
   }; 
 
   ViewTest.prototype.testUpdateIterable = function() {
-    /*:DOC += <div id = "templateIterable">
+    /*:DOC += <div id = "templateCollection">
                 <ul class = "things">
                   <li class = "thing">
                     <span class = "variable"></span>
                   </li>
                 </ul>
               </div>*/
-    var view = new LiveView($("#templateIterable").get(0));
+    var view = new LiveView($("#templateCollection"));
 
-    var thing = view.add("thing", {"variable": "value"});
-    thing.update({"variable": "newValue"});
+    var thing = view.things.add({"variable": "value"});
+    thing.set({"variable": "newValue"});
     assertEquals("Variable was not set", $("#templateIterable .variable").html(), "newValue");
   }; 
-
+  /* FRACK THIS TEST IT WORKS IN THE EXAMPLES WITH THE EXACT SAME CODE */
   ViewTest.prototype.testDeleteIterable = function() {
-    /*:DOC += <div id = "templateIterable">
+    /*:DOC += <div id = "templateDelete">
                 <ul class = "things">
                   <li class = "thing">
                     <span class = "variable"></span>
                   </li>
                 </ul>
               </div>*/
-    var view = new LiveView($("#templateIterable").get(0));
+    var template = new LiveView($("#templateDelete"), {
+      "things": [{"variable": "A variable"}]
+    });
 
-    var thing = view.add("thing", {"variable": "value"});
-    var thing2 = view.add("thing", {"variable": "value2"});
-
-    thing.remove();
-    assertEquals("Not deleted", 1, $("#templateIterable .variable").length);
-    assertEquals("Deleted wrong thing", $("#templateIterable .variable").html(), "value2");
+    template.things.remove(0);
+    assertEquals("Not deleted", 0, $("#templateDelete .thing .variable").length);
   }; 
 
+  ViewTest.prototype.testGetById = function() {
+    /*:DOC += <div id = "templateWitIds">
+                <ul class = "things">
+                  <li class = "thing">
+                    <span class = "variable"></span>
+                  </li>
+                </ul>
+              </div>*/
+    var template = new LiveView($("#templateWitIds"), {
+      "things": [{"variable": "A variable", "id": "ac23f"}, 
+                 {"variable": "Another variable", "id": "5c139"}]
+    });
+
+    var item = template.things.getById("ac23f");
+    assertEquals("Not deleted", "ac23f", item.data.id);
+  }; 
+  ViewTest.prototype.testSort = function() {
+    /*:DOC += <div id = "templateWitIds">
+                <ul class = "things">
+                  <li class = "thing">
+                    <span class = "variable"></span>
+                  </li>
+                </ul>
+              </div>*/
+    var template = new LiveView($("#templateWitIds"), {
+      "things": [{"variable": 1}, 
+                 {"variable": 10},
+                 {"variable": 2},
+                 {"variable": 6}]
+    });
+
+    template.things.sort(function(data1, data2) {
+      return data1.variable - data2.variable;
+    });
+    fail();
+  };       
+  ViewTest.prototype.testAddAfterSort = function() {
+    /*:DOC += <div id = "templateWitIds">
+                <ul class = "things">
+                  <li class = "thing">
+                    <span class = "variable"></span>
+                  </li>
+                </ul>
+              </div>*/
+    var template = new LiveView($("#templateWitIds"), {
+      "things": [{"variable": 1}, 
+                 {"variable": 10},
+                 {"variable": 2},
+                 {"variable": 6}]
+    });
+
+    template.things.sort(function(data1, data2) {
+      return data1.variable - data2.variable;
+    });
+  }; 
 }());
+
