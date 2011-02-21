c58611c90a98522a0fe815eb0eec82c58df2ca51
Zach Smith <x.coder.zach@gmail.com>
2011-02-21 11:43:39 -0600

    added some more tests and made sure that elements always get put back in the right order

--- src/live_view.js
+++ src/live_view.js
@@ -1,4 +1,5 @@
 var LiveView;
+
 (function() {
 
   function each(data, fn, context) {
@@ -14,34 +15,54 @@ var LiveView;
     return Object.prototype.toString.call(array) === '[object Array]';
   };
   
-  function collectPreviousNodes(element, parent) {
-    var prev = [];
-    var el = element.previousSibling;
+  function collectNextNodes(element, parent) {
+    var next = [];
+    var el = element.nextSibling;
     while(el !== null) {
-      prev.push(el);
-      el = el.previousSibling;
+      next.push(el);
+      el = el.nextSibling;
     }
-    return prev;
+    return next;
+  }
+
+  function collectNodes(attribute, element, parent) {
+    var arr = [],
+        el = element[attribute];
+    while(el !== null) {
+      arr.push(el);
+      el = el[attribute];
+    }
+    return arr;
   }
  
   // When reattaching a hidden element we start at the sibling element
   // immediately before it, if that element is still attached, we put
   // our element before it, if there are no elements before it
   // we just prepend it to its parent
-  function reattach(element, parent, siblings) {
+  function reattach(element, parent, next, prev) {
     var i;
-    for(i = 0 ; i < siblings.length ; i++) {
-      $.contains(parent, siblings[i]); 
-      parent.insertBefore(element, siblings[i].nextSibling);
-      return;
+    for(i = 0 ; i < next.length ; i++) {
+      if($.contains(parent, next[i])) {
+        parent.insertBefore(element, next[i]);
+        return;
+      }
     }
-    element.prependTo(parent);
+    for(i = 0 ; i < prev.length ; i++) {
+      if($.contains(parent, prev[i])) {
+        //this is actually insert after
+        parent.insertBefore(element, prev[i].nextSibling);
+        return;
+      }
+    } 
+    // just put the element anywhere, since there are no  visible siblings
+    parent.insertBefore(element, null);
   } 
 
-
+  // Contstructs a new live view from a template (css selector, or html)
+  // and, "optional" data
   LiveView = function(template, data) {
     var that = this;
-    this.context = template;
+    this.context = $(template);
     this.data = data || {};
     this.hiddenElements = {};
     each(data, function(key, value) { 
@@ -53,12 +74,13 @@ var LiveView;
     }, this);
   };
 
-
-
+  // Given the name of the data a user passed in, return an element
+  // to populate with that data
   LiveView.prototype.getElementFromName = function(name, context) {
     return $("." + name, context);
   };
 
+  // Toggles whether a named item is visible on the page
   LiveView.prototype.toggle = function(name) {
     if(this.hiddenElements[name]) {
       this.set(name, true);
@@ -66,19 +88,21 @@ var LiveView;
       this.set(name, false);
     }
   };
-
+  // Sets the values of named element to value, also 
+  // can take an object of name value pairs to bulk set
   LiveView.prototype.set = function(name, value) {
-    // if we're setting a hidden element and it's not already hidden 
-    // unhide it
-    if(this.hiddenElements[name] && value !== false) {
-      var obj = this.hiddenElements[name];
-      reattach(obj.el, obj.par, obj.sibs);
-      delete this.hiddenElements[name];
-    } 
-    var element = this.getElementFromName(name, this.context);
+
     if(arguments.length !== 2) {
       each(name, this.set, this);
     } else {
+      // if we're setting a hidden element and it's not already hidden 
+      // unhide it
+      if(this.hiddenElements[name] && value !== false && value.hidden !== false) {
+        var obj = this.hiddenElements[name];
+        reattach(obj.el, obj.par, obj.next, obj.prev);
+        delete this.hiddenElements[name];
+      } 
+      var element = this.getElementFromName(name, this.context);
       this.data[name] = value;
       if(typeof value === "object") {
         each(value, function(key, value) {
@@ -91,13 +115,15 @@ var LiveView;
       } else if (value === false) {
         var p = element.parent().get(0);
         var e = element.get(0);
-        var s = collectPreviousNodes(e, p);
+        var next = collectNodes("nextSibling", e, p);
+        var prev = collectNodes("previousSibling", e, p);
         element.detach();
         this.hiddenElements[name] = {
-          sibs: s,
+          next: next,
+          prev: prev,
           par: p,
           el: e
-        }
+        };
       } else if(value === true) {
         //we already unhide the element so...do nothing!
       } else {
@@ -126,15 +152,34 @@ var LiveView;
     this.add(data);
   };
 
-
-  LiveViewCollection.prototype.get = function(i) {
-    return this.collection[i];
+  // If one argument, returns view at that index
+  // if two, returns first element with data
+  // where key arg1 === arg2
+  LiveViewCollection.prototype.get = function(index) {
+    var i;
+    if(arguments.length === 2) {
+      var field = arguments[0];
+      var value = arguments[1];
+      for(i = 0 ; i < this.collection.length ; i++) {
+        if(this.collection[i].data[field] === value) {
+          return this.collection[i];
+        }
+      }
+    } else {
+      return this.collection[index];
+    }
   };
 
+  // number of live views in this collection
+  LiveViewCollection.prototype.length = function() {
+    return this.collection.length;
+  };
+ 
   LiveViewCollection.prototype.remove = function(i) {
     return this.collection.splice(i, 1)[0].remove();
   };
 
+  //wipe out everything no more jquery event listeners or associated data
   LiveViewCollection.prototype.removeAll = function() {
     var old = this.collection.splice(0);
     each(old, function(i, item) {
@@ -143,6 +188,7 @@ var LiveView;
     return old;
   }; 
 
+  //detach all event listeners but keep associated jquery data
   LiveViewCollection.prototype.detachAll = function() {
     var old = this.collection.splice(0);
     each(old, function(i, item) {
@@ -151,28 +197,36 @@ var LiveView;
     return old;
   };
 
+  // add an item to the collection if the collection is sorted
+  // adds it in the right place, otherwise add it at the end
+  // return a the new liveView when completed
   LiveViewCollection.prototype.add = function(data) {
-    var element;
+    var element,
+        view;
     if(isArray(data)) {
       each(data, function(i, item) { this.add(item); }, this);
     } else {
       element = this.template.clone(true);
+      view = new LiveView(element, data);
       if(this.currentSortFunction) {
         for(var i = 0 ; i < this.collection.length; i++) {
           if(this.currentSortFunction(this.collection[i].data, data) > 0) {
             element.insertBefore(this.collection[i].context);
-            this.collection.splice(i, 0, new LiveView(element, data));
-            return;
+            this.collection.splice(i, 0, view);
+            return view;
           }
         }
       }
       // if the above loop doesn't return, or the list isn't sorted
       // add it to the end 
       this.container.append(element);
-      this.collection.push(new LiveView(element, data));
+      this.collection.push(view);
+      return view;
     }
   };
 
+  // sort using a function, the function takes two data parameters
+  // bleh....
   LiveViewCollection.prototype.sort = function(fn) {
     this.currentSortFunction = fn;
     var sorted = this.detachAll().sort(function(x, y) {
@@ -184,11 +238,25 @@ var LiveView;
     }, this);
   };
 
-  LiveViewCollection.sortBy = function(feild, isDesc) {
-    this.clear.sort(function(x, y) {
-      return fn(x.data, y.data);
+  // sortBy by is a convenience method, if you don't want
+  // to write your own sort function. Sorts the field given,
+  // alphabetically
+  LiveViewCollection.prototype.sortBy = function(field, isAsc) {
+    isAsc = (isAsc) ? -1 : 1;
+    this.sort(function(x, y) {
+      return x[field].localeCompare(y[field]) * isAsc;
     });
   };
 
-
+  // limit the number of things we show, chop off
+  // anything that doesn't fit!
+  //
+  // example
+  //     collection.limit(10);
+  // would only show the first ten items in a collection
+  // anything else is still in memory, and would show up
+  // if you changed the sort
+  LiveViewCollection.limit = function(count) {
+    throw("not implemented");
+  };
 }());
--- test/view.test.js
+++ test/view.test.js
@@ -41,11 +41,13 @@
                   </li>
                 </ul>
               </div>*/
-    var view = new LiveView($("#templateCollection"));
+    var template = new LiveView("#templateCollection", {
+      "things": []
+    });
 
-    var thing = view.things.add({"variable": "value"});
+    var thing = template.things.add({"variable": "value"});
     thing.set({"variable": "newValue"});
-    assertEquals("Variable was not set", $("#templateIterable .variable").html(), "newValue");
+    assertEquals("Variable was not set", $("#templateCollection .variable").html(), "newValue");
   }; 
   /* FRACK THIS TEST IT WORKS IN THE EXAMPLES WITH THE EXACT SAME CODE */
   ViewTest.prototype.testDeleteIterable = function() {
@@ -77,9 +79,10 @@
                  {"variable": "Another variable", "id": "5c139"}]
     });
 
-    var item = template.things.getById("ac23f");
-    assertEquals("Not deleted", "ac23f", item.data.id);
+    var item = template.things.get("id", "ac23f");
+    assertEquals("Got the right element", "ac23f", item.data.id);
   }; 
+
   ViewTest.prototype.testSort = function() {
     /*:DOC += <div id = "templateWitIds">
                 <ul class = "things">
@@ -90,15 +93,18 @@
               </div>*/
     var template = new LiveView($("#templateWitIds"), {
       "things": [{"variable": 1}, 
-                 {"variable": 10},
+                 {"variable": 3},
                  {"variable": 2},
-                 {"variable": 6}]
+                 {"variable": 4}]
     });
 
     template.things.sort(function(data1, data2) {
       return data1.variable - data2.variable;
     });
-    fail();
+    expectAsserts(4);
+    for(var i = 0 ; i < template.things.length() ; i++) {
+      assertEquals("not sorted", i + 1, template.things.get(i).data.variable);
+    }
   };       
   ViewTest.prototype.testAddAfterSort = function() {
     /*:DOC += <div id = "templateWitIds">
@@ -110,14 +116,61 @@
               </div>*/
     var template = new LiveView($("#templateWitIds"), {
       "things": [{"variable": 1}, 
-                 {"variable": 10},
+                 {"variable": 3},
                  {"variable": 2},
-                 {"variable": 6}]
+                 {"variable": 5}]
     });
 
     template.things.sort(function(data1, data2) {
       return data1.variable - data2.variable;
     });
+
+    template.things.add({"variable": 4});
+
+    expectAsserts(5);
+    for(var i = 0 ; i < template.things.length() ; i++) {
+      assertEquals("not sorted", i + 1, template.things.get(i).data.variable);
+    }
   }; 
+  ViewTest.prototype.testHidingElement = function() {
+    /*:DOC += <div id = "template">
+                <span class = "hideme"></span>
+                <span class = "andme"></span>
+              </div>*/
+    var template = new LiveView($("#template"), {"hideme": false, "andme": false});
+
+    assertEquals("Elements not hidden", 0, $("#template").children().length);
+  }; 
+
+  ViewTest.prototype.testHidingMaintainsOrder = function() {
+    /*:DOC += <div id = "template">
+                text
+                <span class = "hideme"></span>
+                texty
+                <span class = "andme"></span>
+                textyyy
+                <span class = "alsome"></span>
+                textyyyyyy
+                <span class = "ninja"></span>
+                textooooo
+              </div>*/
+    var template = new LiveView($("#template"), {"hideme": 1, "andme": 2, "alsome": 3, "ninja": 4});
+
+    var oldHtml = $("#template").html();
+    //hide in order
+    template.set("hideme", false);
+    template.set("andme", false);
+    template.set("alsome", false);
+    template.set("ninja", false);
+    // make sure they're hidden
+    assertEquals("Elements not hidden", 0, $("#template").children().length);
+    // unhide them!
+    template.set("hideme", true);
+    template.set("ninja", true);
+    template.set("alsome", true);
+    template.set("andme", true);
+    //like we never touched it...KABLAM
+    assertEquals("Html was modified", oldHtml, $("#template").html());
+  };  
 }());
 
